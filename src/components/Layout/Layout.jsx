dff855baa2689b2f982413bf07d621f8c4265eca
reutby <reutby321@gmail.com>
2020-12-10 17:42:40 +0200

    adding the sideDrawer component and taggle menu button

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux/Aux";
 import classes from "./Layout.module.css";
 import ToolBar from "../Navigation/ToolBar/ToolBar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+class Layout extends Component {
 
-const Layout = (props)=>(
-    <Aux>
-    <ToolBar />
-    <main className={classes.Content}>
-    {props.children}
-    </main>
-    </Aux>
-);
+    state={showSideDrawer:false};
+
+    sideDrawerClosedHandler =() =>{
+        this.setState({showSideDrawer:false});
+    }
+
+    sideDrawerToggleHandler = ()=>{
+        this.setState((prev)=>({showSideDrawer: !prev.showSideDrawer}));
+    }
+    render() {
+        return (
+            <Aux>
+                <ToolBar drawerToggle={this.sideDrawerToggleHandler}/>
+                <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>)
+    }
+}
 
 export default Layout;
--- src/components/Navigation/ToolBar/ToolBar.jsx
+++ src/components/Navigation/ToolBar/ToolBar.jsx
@@ -2,11 +2,15 @@ import React from "react"
 import classes from "./ToolBar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-const ToolBar = ()=>(
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+
+const ToolBar = (props) => (
     <header className={classes.ToolBar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
+        <DrawerToggle toggleDrawer = {props.drawerToggle}/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
 
