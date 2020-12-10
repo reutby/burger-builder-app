36fe73e8aeae290e528d604fb07bf8c8c0196095
reutby <reutby321@gmail.com>
2020-12-10 18:08:34 +0200

    improving preformence using shuldbeupdate lifecycle in modal component

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
-const OrderSummary = (props) => {
 
-    const ingredients = Object.keys(props.ingredients)
-        .map(keyIg => {
-            return <li key={keyIg}><span style={{ transform: "capitalize" }}>{keyIg}</span>: {props.ingredients[keyIg]} </li>
-        });
-    return (
+class OrderSummary extends Component {
+    
+    componentDidUpdate() {
+        console.log("[orderSummary] component should update");
+    }
 
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredients}
-            </ul>
-            <h3>Total Price: {props.totalPrice} $</h3>
-            <p>Continue to checkout?</p>
-            <Button
-                btnType="Danger"
-                buttonClicked={ props.purchaseCancel}>CANCEL</Button>
-            <Button
-                btnType="Success"
-                buttonClicked={props.purchaseContinue }>CONTINUE</Button>
-        </Aux>
-    )
+    render() {
+        const ingredients = Object.keys(this.props.ingredients)
+            .map(keyIg => {
+                return <li key={keyIg}><span style={{ transform: "capitalize" }}>{keyIg}</span>: {this.props.ingredients[keyIg]} </li>
+            });
+        return (
+
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredients}
+                </ul>
+                <h3>Total Price: {this.props.totalPrice} $</h3>
+                <p>Continue to checkout?</p>
+                <Button
+                    btnType="Danger"
+                    buttonClicked={this.props.purchaseCancel}>CANCEL</Button>
+                <Button
+                    btnType="Success"
+                    buttonClicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Aux>)
+
+    }
+
 
 }
 
--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.module.css"
 import BackDrop from "../BackDrop/BackDrop";
 import Aux from "../../../hoc/Aux/Aux";
 
-const Modal = (props) => {
-    return (
-        <Aux>
-        <BackDrop show={props.show}/>
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
+class Modal extends Component {
 
-        </Aux>);
+    shouldComponentUpdate(nextProps, nextState) {
+        return (nextProps.show !== this.props.show)
+    }
+    
+    render() {
+
+        return (
+            <Aux>
+                <BackDrop show={this.props.show} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+
+            </Aux>);
+    }
 }
-
 
 export default Modal;
